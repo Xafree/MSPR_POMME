95d6e374bbdb6650b5d5812f953967d9e46705f4
Xafree <emmanueleberst@hotmail.com>
2021-03-29 12:26:11 +0200

    delete useless comment

--- test/api_qr_code_test.dart
+++ test/api_qr_code_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_app/api/api_product.dart';
 import 'package:flutter_app/api/api_qr_code.dart';
-import 'package:flutter_app/view/login_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
@@ -9,14 +8,15 @@ class MockClient extends Mock implements http.Client {}
 
 void main() {
   MockClient mockClient;
-  Login dataResource = new Login();
   ApiProduct product = new ApiProduct();
   ApiQrCode qrCode = new ApiQrCode();
 
   setUp(() {
     mockClient = MockClient();
+  });
 
-    dataResource.createState();
+  tearDown((){
+    mockClient = null;
   });
 
   group('tests', () {
@@ -26,7 +26,6 @@ void main() {
           body: '{"login_mail": "mail3", "password": "pass3"}',
           headers: {"content-type": "application/json"}))
           .thenAnswer((_) async => http.Response('{"status": 200}', 200));
-      //expect(await dataResource.createState().save("mail3","pass3"), 200);
     });
 
     test('Existing Login but Not Matching password', () async {
@@ -35,7 +34,6 @@ void main() {
           body: '{"login_mail": "mail5", "password": "pass1"}',
           headers: {"content-type": "application/json"}))
           .thenAnswer((_) async => http.Response('{"status": 400}', 400));
-      //expect(await dataResource.createState().save("mail5","pass1"), 404);
     });
 
     test('Create space not existing login', () async {
@@ -45,7 +43,6 @@ void main() {
           headers: {
             "content-type": "application/json"
           })).thenAnswer((_) async => http.Response('{"status": 200}', 200));
-      //expect(await dataResource.createState().save(), isA<List>());
     });
 
     test('Create space with existing login', () async {
@@ -55,7 +52,6 @@ void main() {
           headers: {
             "content-type": "application/json"
           })).thenAnswer((_) async => http.Response('{"status": 409}', 409));
-      //expect(await dataResource.createState().save(), isA<List>());
     });
 
     test('No existing login', () async {
@@ -65,7 +61,6 @@ void main() {
           headers: {
             "content-type": "application/json"
           })).thenAnswer((_) async => http.Response('{"status": 404}', 404));
-      //expect(await dataResource.createState().save(), isA<List>());
     });
 
     test('returns all productS ', () async {
@@ -97,15 +92,5 @@ void main() {
       expect(await qrCode.getCodePromos('1'), isA<List>());
     });
 
-    // test('throws an exception if API call was unsuccessful', () {
-    //  final client = MockClient();
-
-    //when(client.get(
-    //      'https://apicoupons.azurewebsites.net/couponresponse/ville/paris'))
-    //.thenAnswer(
-    //  (_) async => http.Response('Response body is empty', 404));
-
-    //  expect(product.getAllProductHtpp(), throwsException);
-    // });
   });
 }
