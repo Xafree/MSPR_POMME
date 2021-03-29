38e7039c6ab27aa5174a230602bfb872b240787d
Xafree <emmanueleberst@hotmail.com>
2021-03-29 09:41:38 +0200

    test d'intégration end bug flutter cant resolve

--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -1,15 +1,14 @@
 import 'dart:io';
-
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-
   group('All app', () {
 
     final login = find.byValueKey('user_email');
     final passWord = find.byValueKey('pass_word');
     final logButton = find.byValueKey('log_button');
+    final scafoldNBarre = find.byTooltip('Open navigation menu');
 
     FlutterDriver driver;
 
@@ -24,22 +23,51 @@ void main() {
     });
 
     test('Login', () async {
-      await driver.tap(login);
-      await driver.enterText("mail3");
-      await driver.tap(passWord);
-      await driver.enterText("pass3");
-      await driver.tap(logButton);
-      sleep(Duration(seconds: 5));
-      //await driver.waitFor(logButton);
-      await driver.getText(find.text('GoStyle'));
+        await driver.tap(login);
+        await driver.enterText("mail3");
+        await driver.tap(passWord);
+        await driver.enterText("pass3");
+        await driver.tap(logButton);
+        sleep(Duration(seconds: 5));
+
+        await driver.getText(find.text('GoStyle'));
     });
 
+    test('drawer', () async {
+      final openDrawer = find.byValueKey('Scaffold');
+      await driver.waitFor(openDrawer);
+      await driver.scroll(openDrawer,-300, 0, Duration(milliseconds: 500));
+      print('clicked on drawer');
+      await driver.getText(find.text('Menu'));
+      sleep(Duration(seconds: 5));
+    });
 
-    test('Navbarre', () async {
+    test('go to scan page', () async {
+      await driver.tap(find.byValueKey('qr_code'));
+      await driver.tap(find.text('Scan coupon'));
+      sleep(Duration(seconds: 5));
+      // await driver.waitFor(find.byValueKey('ButtonScan'));
+      // sleep(Duration(seconds: 5));
+    });
 
-      final mesCoupons = find.byTooltip('goToMesCoupons');
-      await driver.tap(mesCoupons);
-      expect(1, 1);
+    test('go to MesCoupons', () async {
+      final openDrawer = find.byValueKey('drawerBar');
+      await driver.waitFor(openDrawer);
+      await driver.scroll(openDrawer,-600, 0, Duration(milliseconds: 500));
+      await driver.getText(find.text('Menu'));
+      sleep(Duration(seconds: 2));
+      await driver.tap(find.byValueKey('Mes coupons'));
+      sleep(Duration(seconds: 5));
+    });
+
+    test('to go deconnection', () async {
+      final openDrawer = find.byValueKey('drawerBar');
+      await driver.waitFor(openDrawer);
+      await driver.scroll(openDrawer,600, 0, Duration(milliseconds: 500));
+      await driver.getText(find.text('Menu'));
+      sleep(Duration(seconds: 2));
+      await driver.tap(find.byValueKey('deconnexion'));
+      //sleep(Duration(seconds: 5));
     });
   });
 }
